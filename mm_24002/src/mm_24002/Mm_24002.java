3d9920fbc35ce26a52fc0cb5fb7a7d7247f225a9
Alexismzrg <mm24002@ues.edu.sv>
2025-05-09 12:04:23 -0600

    Update Mm_24002.java

--- mm_24002/src/mm_24002/Mm_24002.java
+++ mm_24002/src/mm_24002/Mm_24002.java
@@ -14,7 +14,7 @@ public class Mm_24002 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.print("Hello world IIS");
     }
     
 }
